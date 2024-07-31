39d62cb7cb78c63c1570f11bfedf75298683b833
PAPPU KUMAR <122831860+pappukr034@users.noreply.github.com>
2024-07-31 10:26:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app=express();
 app.use(express.json())
 
 app.use(cors({
-  origin:'https://skillrisefrontend.onrender.com/',
+  origin:'https://skillrisefrontend.onrender.com',
   credentials:true
 }));
   
